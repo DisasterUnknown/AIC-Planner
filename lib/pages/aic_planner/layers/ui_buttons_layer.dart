291a775b6691e101ee73b783aa23d5b6442cbc84
priyashantha <priyashanthafdo26@gmail.com>
2026-02-02 10:58:41 +0530

    feat: Add facility registry and facility list for game mechanics
    - Created `facility_registry.dart` to define various facility types with attributes such as power, tier, skill, and description.
    - Introduced `facility_registry_list.dart` to compile a list of all facilities for easy access and management within the game.

--- lib/pages/aic_planner/layers/ui_buttons_layer.dart
+++ lib/pages/aic_planner/layers/ui_buttons_layer.dart
@@ -1,5 +1,6 @@
 import 'package:aic_planner/pages/aic_planner/model/facility_instance.dart';
-import 'package:aic_planner/shared/data/registry/facility_registry.dart';
+import 'package:aic_planner/shared/data/constants.dart';
+import 'package:aic_planner/shared/data/registry/facility_registry/facility_registry.dart';
 import 'package:aic_planner/shared/widget/corner_back_button.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +14,78 @@ class UIButtonsLayer extends StatelessWidget {
     return Stack(
       children: [
         cornerBackBtn(context),
+
+        // Top save and infor btn area
+        Positioned(
+          top: -1,
+          right: -1,
+          child: Column(
+            children: [
+              Container(
+                height: 40,
+                width: 165,
+                decoration: BoxDecoration(
+                  color: AppCustomColors.secondaryUI.withValues(alpha: 0.7),
+                  border: Border.all(color: Colors.white54, width: 2),
+                ),
+                child: Center(child: Text('v1.0.0')),
+              ),
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 55,
+                      width: 55,
+                      decoration: BoxDecoration(
+                        color: AppCustomColors.secondaryUI.withValues(
+                          alpha: 0.7,
+                        ),
+                        border: Border.all(color: Colors.white54, width: 2),
+                      ),
+                      child: Center(
+                        child: Icon(Icons.save, color: Colors.white, size: 24),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 55,
+                      width: 55,
+                      decoration: BoxDecoration(
+                        color: AppCustomColors.secondaryUI.withValues(
+                          alpha: 0.7,
+                        ),
+                        border: Border.all(color: Colors.white54, width: 2),
+                      ),
+                      child: Center(
+                        child: Icon(Icons.account_tree, color: Colors.white, size: 24),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 55,
+                      width: 55,
+                      decoration: BoxDecoration(
+                        color: AppCustomColors.secondaryUI.withValues(
+                          alpha: 0.7,
+                        ),
+                        border: Border.all(color: Colors.white54, width: 2),
+                      ),
+                      child: Center(
+                        child: Icon(Icons.info, color: Colors.white, size: 24),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+
         Positioned(
           bottom: 20,
           left: 20,
